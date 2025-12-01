7f88e1ba347172ce449802dde6063cd7a4ef07e3
Ivan Chernov <i.chernov@softlist.ua>
2025-12-01 19:57:31 +0000

    Add migrations. Inject Infrastructure layer to Main

---
+++ backend/src/HttpTaskService.Infrastructure/Migrations/20251201195542_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HttpTaskService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "http_tasks",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    url = table.Column<string>(type: "character varying(2048)", maxLength: 2048, nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    started_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    completed_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    status_code = table.Column<int>(type: "integer", nullable: true),
+                    content_length = table.Column<long>(type: "bigint", nullable: true),
+                    duration_ms = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_http_tasks", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_HttpTasks_CreatedAt",
+                table: "http_tasks",
+                column: "created_at");
+
+            migrationBuilder.CreateIndex(
+                name: "IDX_HttpTasks_Status",
+                table: "http_tasks",
+                column: "status");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "http_tasks");
+        }
+    }
+}
